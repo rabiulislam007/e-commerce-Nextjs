8775d4be49bea998094bc12be9e3db33bb28b53d
Md Rabi Ul Uslam <58680120+rabiulislam007@users.noreply.github.com>
2023-07-05 13:18:18 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Shopping World',
+  title: 'E-Commerce Store',
   description: 'Ultimate Shopping app',
 }
 
